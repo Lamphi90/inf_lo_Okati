4d6d63583e9c401d80ce93db01d43375e10a67e2
Lakshmi Priyanka Chirumamilla <34736099+Lamphi90@users.noreply.github.com>
2017-12-21 00:32:23 -0800

    Add files via upload

---
+++ Lakshmi_Chirumamilla_task2.scala
@@ -0,0 +1,36 @@
+package com.inf.homework
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object Lakshmi_Chirumamilla_task2 {
+    def main(args: Array[String]){
+      
+    val conf = new SparkConf().setAppName("Word Count").setMaster("local")
+    val sparkContext = new SparkContext(conf)
+    if (args.length <3) 
+      {
+      println("Please provide sufficient arguments from command line as per readMe")
+      System.exit(1)
+      }
+    
+    val usersDat = sparkContext.textFile(args(0))
+    val  ratingsDat = sparkContext.textFile(args(1))
+    val  moviesDat = sparkContext.textFile(args(2))
+   
+    val split_Users = usersDat.map(line=>line.split("\\::")).map(line=>(line(0).toInt, line(1).toString))
+    val split_Ratings = ratingsDat.map(line=>line.split("\\::")).map(line=>(line(0).toInt, line(1).toInt,line(2).toDouble))
+    val split_Movies = moviesDat.map(line=>line.split("\\::")).map(line=>(line(0).toInt, line(2).toString))
+    
+    val user_items = split_Users.map(x=>(x._1,x))
+    val rating_items = split_Ratings.map(x=>(x._1,x))
+    val movie_items = split_Movies.map(x=>(x._1,x))
+    
+    var join_1 = user_items.join(rating_items).map(t=>((t._2._2._2),( t._2._1._2,t._2._2._3))) 
+     var result = movie_items.join(join_1).map(t=>((t._2._1._2, t._2._2._1),(t._2._2._2))).mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues(y => 1.0 * y._1 / y._2).sortByKey().collect
+    sparkContext.parallelize(result.map { t =>(""+t._1._1+","+t._1._2+","+t._2+"")}).saveAsTextFile("Lakshmi_Chirumamilla_result_task2.txt")
+
+   
+    
+    sparkContext.stop()
+    }
+}
